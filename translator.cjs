18018c609fccf1883961af8054275c063dc83593
Josh <125091065+joshxviii@users.noreply.github.com>
2025-05-31 13:18:16 -0400

    allow for some html in translations for style

--- translator.cjs
+++ translator.cjs
@@ -22,17 +22,50 @@ loadLanguage();
 // get translation
 function translate(key) { return translations[key] || (defaultTranslations[key] || `${key}`); }
 
+function updateElementTranslation(el) {
+    const key = el.getAttribute('translation');
+    if (!key) return;
+
+    if (el.type === 'text') {
+        el.setAttribute('placeholder', translate(key));
+        return;
+    }
+    if (el.type === 'button') {
+        el.setAttribute('value', translate(key));
+        return;
+    }
+
+    Array.from(el.childNodes).forEach(node => {
+        if (
+            (node.nodeType === Node.ELEMENT_NODE && node.classList.contains('translation-html')) ||
+            node.nodeType === Node.TEXT_NODE
+        ) el.removeChild(node);
+    });
+
+    const temp = document.createElement('span');
+    temp.className = 'translation-html';
+    temp.innerHTML = translate(key);
+    el.appendChild(temp);
+}
+
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        updateElementTranslation(mutation.target);
+    });
+});
+
 // update translations for all elements on the document
 function updateHtmlDocumentTranslations() {
-    document.querySelectorAll('[translation]').forEach(el => {
-        const key = el.getAttribute('translation');
-        if (key) {
-            if (el.type === 'text') el.setAttribute('placeholder', translate(key))
-            else if (el.type === 'button') el.setAttribute('value', translate(key))
-            else el.innerHTML = translate(key);
-        }
+    observer.disconnect();
+
+    document.querySelectorAll('[translation]').forEach(el => updateElementTranslation(el));
+
+    observer.observe(document, {
+        subtree: true,
+        attributeFilter: ['translation']
     });
 }
+
 
 module.exports = {
     loadLanguage,
